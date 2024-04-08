2f1ddf6ff23f714e48c0b99f6a8e412da09e7879
Driton <106562685+DritonLatifi@users.noreply.github.com>
2024-04-08 12:24:36 +0000

    fixed movie titles breaking picture height

--- src/components/slider.js
+++ src/components/slider.js
@@ -10,8 +10,8 @@ export default function Slider({ array }) {
         array.map(movie => {
             return (
                 <Link href={`/${movie.id}`} key={movie.id} className='flex flex-col gap-2 min-w-[42.5%]'>
-                    <Image key={movie.id} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} height={800} width={800} alt="movie" className="rounded-md object-cover aspect-square w-full h-full" />
-                    <span className='break-words'>{movie.title}</span>
+                    <Image key={movie.id} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} height={600} width={600} alt="movie" className="rounded-md object-cover aspect-square w-full h-full" />
+                    <span className='overflow-hidden whitespace-nowrap'>{movie.title}</span>
                     <div className='flex items-center gap-2'>
                         <Rating rating={movie.vote_average} />
                     </div>
