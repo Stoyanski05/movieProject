ae3ac5a12039f12d22066654ca28bbb3ab227f22
Driton <106562685+DritonLatifi@users.noreply.github.com>
2024-04-08 06:37:29 +0000

    every cast member takes 23% width

--- src/components/person.js
+++ src/components/person.js
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function Person({src, name}) {
     return (
-        <div className="flex flex-col gap-2 w-[100px] h-[175px]">
+        <div className="flex flex-col gap-2 w-[23%] h-1/2">
             <Image src={src} width={100} height={100} alt="actor" className="rounded-lg aspect-square object-cover"/>
             <span className="w-fit">{name}</span>
         </div>
