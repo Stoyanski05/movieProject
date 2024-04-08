dd177b0664b01f82110b46b809e0dbeeb478e7e2
Driton <106562685+DritonLatifi@users.noreply.github.com>
2024-04-08 08:24:11 +0000

    person name x and y overflow hidden

--- src/components/person.js
+++ src/components/person.js
@@ -4,9 +4,9 @@ import Image from "next/image"
 
 export default function Person({src, name}) {
     return (
-        <div className="flex flex-col gap-2 w-[23%] h-1/2">
+        <div className="flex flex-col gap-2 w-[23%] h-1/2 overflow-hidden">
             <Image src={src} width={100} height={100} alt="actor" className="rounded-lg aspect-square object-cover"/>
-            <span className="w-fit">{name}</span>
+            <span className="w-fit h-12">{name}</span>
         </div>
     )
 }
--- src/components/slider.js
+++ src/components/slider.js
@@ -10,7 +10,7 @@ export default function Slider({ array }) {
         array.map(movie => {
             return (
                 <Link href={`/${movie.id}`} key={movie.id} className='flex flex-col gap-2 min-w-[42.5%]'>
-                    <Image key={movie.id} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} height={200} width={200} alt="movie" className="rounded-md object-cover aspect-square w-full h-full" />
+                    <Image key={movie.id} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} height={800} width={800} alt="movie" className="rounded-md object-cover aspect-square w-full h-full" />
                     <span className='break-words'>{movie.title}</span>
                     <div className='flex items-center gap-2'>
                         <Rating rating={movie.vote_average} />
