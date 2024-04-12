374b4ffbf5ae7b01581ef6b72648878f35510915
DritonLatifi <driton.latifi101@gmail.com>
2024-04-12 10:25:40 +0200

    sun & moon toggle

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -2,6 +2,14 @@
 
 import { useEffect, useState } from "react"
 
+import Image from 'next/image'
+
+import Moon from '/public/moon.png'
+import Sun from '/public/sun.png'
+
+import BackgroundMoon from '/public/backgroundMoon.jpg'
+import BackgroundSun from '/public/backgroundSun.jpg'
+
 export default function Toggle({ element }) {
     const [darkmode, setDarkmode] = useState(false)
     const [isChecked, setIsChecked] = useState(false)
@@ -24,9 +32,10 @@ export default function Toggle({ element }) {
     }, [isChecked])
 
     return (
-        <label htmlFor="checkbox" className={`flex items-center cursor-pointer w-14 h-8 rounded-full  ${isChecked ? 'bg-white' : 'bg-[#AAA9B1]'}`} >
+        <label htmlFor="checkbox" className={`flex items-center cursor-pointer w-14 h-8 rounded-full`} >
             <input type="checkbox" id="checkbox" className="sr-only" checked={isChecked} onChange={checkHandler} />
-            <div className={`w-6 h-6 relative rounded-full shadow-md transition ${isChecked ? 'bg-black translate-x-7' : 'bg-white translate-x-1'}`}></div>
+            <Image src={isChecked ? Moon : Sun} alt="toggle icon" className={`rounded-full transition duration-150 absolute object-cover ${isChecked ? 'translate-x-7' : 'translate-x-1'}`} width={30} height={30}/>
+            <Image className="overflow-hidden rounded-full" src={isChecked ? BackgroundMoon : BackgroundSun}/>
         </label>
     )
 }
