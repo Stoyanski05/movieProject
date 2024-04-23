3c74cac4e447f6d08e4ee71fcca6dda3c3b3a3b4
DritonLatifi <driton.latifi101@gmail.com>
2024-04-23 12:05:37 +0200

    .

--- src/app/browse/page.js
+++ src/app/browse/page.js
@@ -1,21 +1,64 @@
 "use client"
 
 import Footer from "@/components/footer"
+import Link from "next/link"
+import Image from "next/image"
+import { useEffect, useRef, useState } from "react"
 
 import { FaSearchengin } from "react-icons/fa6"
+import Toggle from "@/components/toggle"
+import { useSearchParams } from "next/navigation"
 
 export default function Home() {
+    const [movies, setMovies] = useState([])
+    const [inputValue, setInputValue] = useState('') // set value
+    const ref = useRef(null)
+    const formRef = useRef(null)
+
+    useEffect(() => {
+        if (searchParams.get('query')) {
+            // set here
+        }
+    }, [])
+
+    const searchParams = useSearchParams()
+
+    async function formSubmit(e) {
+        e.preventDefault()
+
+        const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${e.target[0].value}&api_key=d61d03c4897622853f09d1e0b7a41c5b`, { cache: "force-cache" })
+        const data = await res.json()
+
+        data.results.sort((a, b) => b.vote_average - a.vote_average)
+
+        setMovies(data.results)
+    }
+
     return (
-        <>
-            <main className="h-[50vh] flex justify-center mt-4">
-                <form action="/browse?search=">
+        <div ref={ref}>
+            <header className="h-[8vh] w-full flex justify-center items-center">
+                <form ref={formRef} onSubmit={formSubmit}>
                     <label className="flex justify-center items-center gap-4 py-2 px-2 border-black border-2 bg-black text-white h-fit w-fit">
                         <input className="outline-none bg-inherit" placeholder="Fight Club.."></input>
                         <FaSearchengin size={30} />
                     </label>
                 </form>
+                <div className="absolute top-5 right-4">
+                    {ref && ref.current && <Toggle element={ref.current} />}
+                </div>
+            </header>
+            <main className="h-[50vh] flex flex-col gap-20 items-center mt-4">
+                <article className="flex flex-wrap gap-4 items-center mb-20 px-10 w-full">
+                    {movies.map(movie => {
+                        return (
+                            <Link href={`/${movie.id}`} key={movie.id} className='flex flex-col gap-2 w-40'>
+                                <Image priority key={movie.id} src={movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : '/person-placeholder.jpg'} height={600} width={600} alt="movie" className="rounded-md object-cover aspect-square w-full h-full" />
+                            </Link>
+                        )
+                    })}
+                </article>
             </main>
             <Footer page={'browse'} />
-        </>
+        </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,8 @@ import Slider from "@/components/slider";
 
 import { useEffect, useRef, useState } from "react";
 
+import { useRouter } from "next/navigation";
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -19,13 +21,15 @@ import { useSearchParams } from "next/navigation";
 export default function Home() {
   const [popular, setPopular] = useState([])
   const [trending, setTrending] = useState([])
+  const [searchResult, setSearchResult] = useState([])
 
   const ref = useRef(null)
+  const searchRef = useRef(null)
 
   const searchParams = useSearchParams()
+  const router = useRouter()
 
   useEffect(() => {
-
     (async () => {
       await fetch('https://api.themoviedb.org/3/authentication/session/new', {
         method: 'POST',
@@ -61,10 +65,50 @@ export default function Home() {
     })()
   }, [])
 
+  async function changeHandler(e) {
+    e.target.value.length > 0 ? searchRef.current.style.height = '500px' : searchRef.current.style.height = '0px'
+
+    const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${e.target.value}&api_key=d61d03c4897622853f09d1e0b7a41c5b`, { cache: "force-cache" })
+    const data = await res.json()
+
+    data.results.sort((a, b) => b.vote_average - a.vote_average)
+
+    setSearchResult(data.results)
+  }
+
+  function redirectHandler(e) {
+    if (e.code == 'Enter') router.push('http://localhost:3000/browse?query=' + e.target.value)
+  }
+
   return (
     <div ref={ref} className="bg-white">
       <header className="h-[100px] mb-2">
-        <div className="flex justify-between h-full items-center w-2/3 px-6 float-right">
+        <div className="flex justify-between h-full items-center w-full px-6 float-right">
+          <div className="relative flex flex-col w-fit">
+            <input type="search" onChange={changeHandler} onKeyUp={redirectHandler} className="bg-inherit w-full" placeholder="Fight Club.."></input>
+            <div ref={searchRef} className="absolute flex flex-col w-fit mt-7 bg-black transition-all duration-1000 text-white overflow-y-scroll overflow-x-hidden">
+              {searchResult.map((movie) => {
+                return (
+                  <Link href={`/${movie?.id}`} key={movie?.id} className="flex gap-2 h-40">
+                    <Image src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`} height={100} width={120} className="rounded-md object-cover" alt="movie" />
+                    <div className="flex flex-col justify-evenly w-3/4">
+                      <h2 className="font-bold">{movie?.title}</h2>
+                      <div className="flex items-center gap-2">
+                        <Rating rating={movie?.vote_average} />
+                      </div>
+                      <div className={`flex flex-nowrap gap-2 pr-12 ${movie?.genre_ids.length > 3 ? '' : 'overflow-x-hidden'} overflow-y-hidden`}>
+                        <Genre ids={movie?.genre_ids} />
+                      </div>
+                      <span className="flex items-center gap-2">
+                        <FaClock />
+                        1h 10min
+                      </span>
+                    </div>
+                  </Link>
+                )
+              })}
+            </div>
+          </div>
           <h1 className="text-2xl font-bold">MyMovie</h1>
           {ref.current && <Toggle element={ref.current} />}
         </div>
