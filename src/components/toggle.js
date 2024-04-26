6a9d681ca72403f358cf6872a2f2f37d87c6053e
Stoyanski <danchevstoyan@gmail.com>
2024-04-26 09:01:52 +0200

    billeder

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -28,9 +28,6 @@ export default function Toggle({ element }) {
     return (
         <label htmlFor="checkbox" className={`flex items-center cursor-pointer w-14 h-8 rounded-full`} >
             <input type="checkbox" id="checkbox" className="sr-only" checked={isChecked} onChange={checkHandler} />
-            <Image src='/Sun.png' alt="toggle icon" className={`rounded-full transition duration-500 absolute object-cover ${isChecked ? 'translate-x-6 opacity-0' : 'translate-x-0 opacity-100'}`} width={30} height={30}/>
-            <Image src='/moon.png' alt="toggle icon" className={`rounded-full transition duration-500 absolute object-cover ${isChecked ? 'translate-x-6 opacity-100' : 'translate-x-0 opacity-0'}`} width={30} height={30}/>
-            <Image className="overflow-hidden rounded-full" alt="background" src={isChecked ? '/backgroundMoon.jpg' : '/backgroundSun.jpg'} width={100} height={100}/>
         </label>
     )
 }
